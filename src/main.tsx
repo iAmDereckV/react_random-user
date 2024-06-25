f0d4bdf1d2f1c1b0d221571741d6c029442594ca
iAmDereck <dereck.vivas121@est.unanleon.edu.ni>
2024-06-24 19:43:02 -0600

    instalaciones de consola basicas

---
+++ src/main.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+
+// CONSOLE
+// npm install axios formik yup
+// // npm install @mui/icons-material @mui/material @emotion/styled @emotion/react
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
